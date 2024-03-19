79a802cc3347f4c2eb761ca95b44013e11d56a1f
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-03-19 19:19:05 +0530

    Java Solutions

--- Java/Java Stdin and Stdout I.java
+++ Java/Java Stdin and Stdout I.java
@@ -1,12 +1,9 @@
 import java.util.*;
 
 public class Solution {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-
         System.out.println(a);
-
     }
 }
