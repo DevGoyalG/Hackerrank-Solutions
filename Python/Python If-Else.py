d0d485a26ead76f3aee56fafc3d504186103c999
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-03-19 20:03:56 +0530

    Python Solutions

--- Python/Python If-Else.py
+++ Python/Python If-Else.py
@@ -1,10 +1,15 @@
-if __name__ == '__main__':
-    n = int(input().strip())    
-    if n%2 != 0:
-        print("Weird")
-    elif n%2 == 0 and n>2 and n<=5:
-        print("Not Weird")
-    elif n%2 ==0 and n > 6 and n <=20:
-        print("Weird")
-    else:
-        print("Not Weird")
+import math 
+import os 
+import random 
+import re 
+import sys 
+n = int(input().strip())
+
+if(n % 2 != 0): 
+    print("Weird")
+elif(n>20): 
+    print("Not Weird") 
+elif(n > 5): 
+    print("Weird") 
+else: 
+    print("Not Weird")
