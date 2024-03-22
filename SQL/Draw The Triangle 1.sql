224a233e3a74862e274d3089807c58c252742ac9
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-03-22 17:14:42 +0530

    SQL Solutions

---
+++ SQL/Draw The Triangle 1.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+
+CREATE PROCEDURE iterateAndPrint() 
+BEGIN 
+DECLARE start INT DEFAULT 20; 
+DECLARE checkVar INT; 
+WHILE start > 0 
+DO
+
+SELECT REPEAT("* ", start); 
+SET start = start - 1; 
+END WHILE;
+
+END //
+
+DELIMITER ;
+CALL iterateAndPrint();
